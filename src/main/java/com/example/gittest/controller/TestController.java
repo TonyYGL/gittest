01683f451392c8a728986a835a633dd1d5b23bcd
tony_ykliu <tony_ykliu@tutorabc.com>
2022-07-15 10:28:42 +0800

    fix reuqest mapping issue

--- src/main/java/com/example/gittest/controller/TestController.java
+++ src/main/java/com/example/gittest/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.example.gittest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/test")
+@RestController
+@RequestMapping("/test")
 public class TestController {
 
     @GetMapping("/hello")
